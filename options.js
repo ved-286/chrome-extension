4bf1d500db60653b4a3872d736bcec178588d29d
unknown <patelved1921@gmail.com>
2024-12-01 00:08:41 -0800

    tried some storge chrome apis but there is some error

--- options.js
+++ options.js
@@ -1,6 +1,3 @@
 const inpt = document.getElementById("input-text")
 const btn = document.getElementById("btn")
 
-btn.addEventListener("click",()=>{
-    console.log(inpt.value)
-})
--- popup.js
+++ popup.js
@@ -2,9 +2,3 @@ const TimeElement = document.getElementById("time")
 const CurrentTime = new Date().toLocaleTimeString()
 TimeElement.textContent =`the Time Is ${CurrentTime}`
 
-chrome.action.setBadgeText({
-    text:"Time",
-},()=>{
-    console.log("time badge is here");
-}
-  )
